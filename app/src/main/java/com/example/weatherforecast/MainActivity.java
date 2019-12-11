713249c0efd7a4cd602e7f1b855cbc8c3b619392
DELL <manzoorcm96@gmail.com>
2019-12-11 10:28:26 +0530

    Initial commit

---
+++ app/src/main/java/com/example/weatherforecast/MainActivity.java
@@ -0,0 +1,36 @@
+package com.example.weatherforecast;
+
+import androidx.appcompat.app.AppCompatActivity;
+import androidx.recyclerview.widget.LinearLayoutManager;
+import androidx.recyclerview.widget.RecyclerView;
+
+import android.os.Bundle;
+
+import java.util.LinkedList;
+
+public class MainActivity extends AppCompatActivity {
+    LinkedList<String> mWordList=new LinkedList<String>();
+    private RecyclerView mRecyclerView;
+    private WordListAdapter mAdapter;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+        addValues();
+
+        mRecyclerView = findViewById(R.id.recyclerview);
+        mAdapter = new WordListAdapter(this, mWordList);
+        mRecyclerView.setAdapter(mAdapter);
+        mRecyclerView.setLayoutManager(new LinearLayoutManager(this));
+    }
+
+    void addValues()
+    {
+        for(int i=0;i<10;i++)
+        {
+            mWordList.addLast("Weather data"+i);
+        }
+
+    }
+}
---
+++ app/src/main/java/com/example/weatherforecast/WordListAdapter.java
@@ -0,0 +1,70 @@
+package com.example.weatherforecast;
+
+import android.content.Context;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+import android.widget.TextView;
+
+import androidx.annotation.NonNull;
+import androidx.recyclerview.widget.RecyclerView;
+
+import java.util.LinkedList;
+
+public class WordListAdapter extends RecyclerView.Adapter<WordListAdapter.WordViewHolder>
+{
+    private final LinkedList<String> mWordList;
+    private LayoutInflater mInflater;
+
+    public WordListAdapter(Context context, LinkedList<String> mWordList) {
+        mInflater=LayoutInflater.from(context);
+        this.mWordList = mWordList;
+    }
+
+    @NonNull
+    @Override
+    public WordListAdapter.WordViewHolder onCreateViewHolder(@NonNull ViewGroup parent, int viewType) {
+        View mItemView = mInflater.inflate(R.layout.recylerview_item,
+                parent, false);
+        return new WordViewHolder(mItemView, this);
+    }
+
+    @Override
+    public void onBindViewHolder(@NonNull WordListAdapter.WordViewHolder holder, int position) {
+
+        String mCurrent = mWordList.get(position);
+        holder.wordItemView.setText(mCurrent);
+    }
+
+    @Override
+    public int getItemCount() {
+        return mWordList.size();
+    }
+
+    public class WordViewHolder extends RecyclerView.ViewHolder implements View.OnClickListener{
+        public final TextView wordItemView;
+        final WordListAdapter mAdapter;
+        public WordViewHolder(@NonNull View itemView, WordListAdapter mAdapter) {
+            super(itemView);
+            this.wordItemView = itemView.findViewById(R.id.textView);
+            this.mAdapter = mAdapter;
+            itemView.setOnClickListener(this);
+        }
+
+        @Override
+        public void onClick(View view) {
+
+            int mPosition = getLayoutPosition();
+            String element = mWordList.get(mPosition);
+            mWordList.set(mPosition,element+" clicked!");
+            mAdapter.notifyDataSetChanged();
+
+
+
+
+
+
+
+        }
+    }
+}
